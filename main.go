9de8146e04a1cee3b944a1958ccf887de069d139
Peter Wong <impeterwong@hotmail.com>
2018-05-22 02:30:53 +0800

    done

--- main.go
+++ main.go
@@ -1,18 +1,65 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strings"
 
 	"github.com/coreos/go-semver/semver"
 	"github.com/google/go-github/github"
 )
 
+// for sorting slice with version
+type VersionSlice []*semver.Version
+
+func (s VersionSlice) Len() int           { return len(s) }
+func (s VersionSlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s VersionSlice) Less(i, j int) bool { return s[i].LessThan(*s[j]) }
+
 // LatestVersions returns a sorted slice with the highest version as its first element and the highest version of the smaller minor versions in a descending order
 func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*semver.Version {
 	var versionSlice []*semver.Version
-	// This is just an example structure of the code, if you implement this interface, the test cases in main_test.go are very easy to run
+
+	for _, releasedVersion := range releases {
+		if minVersion.LessThan(*releasedVersion) {
+			// versionSlice = append(versionSlice, releasedVersion)
+			versionSlice = insertVersion(versionSlice, releasedVersion)
+		}
+	}
+
+	sort.Sort(sort.Reverse(VersionSlice(versionSlice)))
+
 	return versionSlice
+}
+
+//insert version and make sure the biggest patch of each version is inserted
+func insertVersion(currentSlice []*semver.Version, toInsert *semver.Version) []*semver.Version {
+	var resultSlice []*semver.Version
+
+	compared := false
+
+	for _, sliceVersion := range currentSlice {
+		if sliceVersion.Major != toInsert.Major || sliceVersion.Minor != toInsert.Minor {
+			resultSlice = append(resultSlice, sliceVersion)
+		} else if toInsert.LessThan(*sliceVersion) {
+			resultSlice = append(resultSlice, sliceVersion)
+			compared = true
+		} else {
+			resultSlice = append(resultSlice, toInsert)
+			compared = true
+		}
+	}
+
+	if compared == false {
+		resultSlice = append(resultSlice, toInsert)
+		return resultSlice
+	}
+
+	return resultSlice
 }
 
 // Here we implement the basics of communicating with github through the library as well as printing the version
@@ -20,24 +67,51 @@ func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*s
 // Please use the format defined by the fmt.Printf line at the bottom, as we will define a passing coding challenge as one that outputs
 // the correct information, including this line
 func main() {
+	//open file
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, provide file name!")
+		return
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
 	// Github
 	client := github.NewClient(nil)
 	ctx := context.Background()
 	opt := &github.ListOptions{PerPage: 10}
-	releases, _, err := client.Repositories.ListReleases(ctx, "kubernetes", "kubernetes", opt)
-	if err != nil {
-		panic(err) // is this really a good way?
-	}
-	minVersion := semver.New("1.8.0")
-	allReleases := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		versionString := *release.TagName
-		if versionString[0] == 'v' {
-			versionString = versionString[1:]
-		}
-		allReleases[i] = semver.New(versionString)
-	}
-	versionSlice := LatestVersions(allReleases, minVersion)
 
-	fmt.Printf("latest versions of kubernetes/kubernetes: %s", versionSlice)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		stringSlice := strings.Split(scanner.Text(), ",") // the line
+
+		if stringSlice[0] == "repository" && stringSlice[1] == "min_version" {
+			continue
+		}
+
+		packageSlice := strings.Split(stringSlice[0], "/")
+
+		releases, _, err := client.Repositories.ListReleases(ctx, packageSlice[0], packageSlice[1], opt)
+		if err != nil {
+			log.Fatal(err)
+			// panic(err) // is this really a good way?
+		}
+		minVersion := semver.New(stringSlice[1])
+		allReleases := make([]*semver.Version, len(releases))
+		for i, release := range releases {
+			versionString := *release.TagName
+			if versionString[0] == 'v' {
+				versionString = versionString[1:]
+			}
+			allReleases[i] = semver.New(versionString)
+		}
+		versionSlice := LatestVersions(allReleases, minVersion)
+
+		fmt.Printf("latest versions of %s: %s\n", stringSlice[0], versionSlice)
+	}
+
 }
--- main_test.go
+++ main_test.go
@@ -46,13 +46,36 @@ func TestLatestVersions(t *testing.T) {
 		{
 			versionSlice:   []string{"2.2.1", "2.2.0"},
 			expectedResult: []string{"2.2.1"},
-			minVersion:     semver.New("2.2.1"),
+			minVersion:     semver.New("2.2.0"),
 		},
-		// Implement more relevant test cases here, if you can think of any
+		{
+			versionSlice:   []string{"1.10.1", "2.10.1", "2.10.2", "1.10.2", "3.10.1", "1.8.9", "1.9.5"},
+			expectedResult: []string{"3.10.1", "2.10.2", "1.10.2"},
+			minVersion:     semver.New("1.10.0"),
+		},
+		{
+			versionSlice:   []string{"2.2.1", "2.2.0", "2.2.0-alpha.2", "2.2.0-alpha.0", "2.2.0-beta.0"},
+			expectedResult: []string{"2.2.1"},
+			minVersion:     semver.New("2.1.0"),
+		},
+		{
+			versionSlice:   []string{"2.2.1", "2.2.0"},
+			expectedResult: []string{"2.2.1"},
+			minVersion:     semver.New("2.2.0"),
+		},
+		{
+			versionSlice:   []string{"1.11.0-alpha.2", "1.11.0-alpha.1", "1.10.3", "1.10.2", "1.10.1", "1.9.7", "1.8.13", "1.8.12", "1.8.11", "1.7.16"},
+			expectedResult: []string{"1.11.0-alpha.2", "1.10.3", "1.9.7", "1.8.13"},
+			minVersion:     semver.New("1.8.0"),
+		},
 	}
 
 	test := func(versionData []string, expectedResult []string, minVersion *semver.Version) {
 		stringSlice := versionToStringSlice(LatestVersions(stringToVersionSlice(versionData), minVersion))
+		if len(stringSlice) != len(expectedResult) {
+			t.Errorf("Size of the result is different, Received %s, expected %s", stringSlice, expectedResult)
+			return
+		}
 		for i, versionString := range stringSlice {
 			if versionString != expectedResult[i] {
 				t.Errorf("Received %s, expected %s", stringSlice, expectedResult)
